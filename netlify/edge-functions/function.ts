51761036ca9837afef0b23f7c36221494c7fc370
edgeone pages <simxin@tencent.com>
2025-06-26 19:59:05 +0800

    update

--- netlify/edge-functions/function.ts
+++ netlify/edge-functions/function.ts
@@ -1,6 +1,7 @@
-import * as cfg from '../config';
+// import * as cfg from '../config';
 
 export default async function handler(req: Request) {
-    cfg.setName(cfg.getName() + 'sim111');
-    return new Response(`edge function netlify ${cfg.getName()}`);
+    // cfg.setName(cfg.getName() + 'sim111');
+    // return new Response(`edge function netlify ${cfg.getName()}`);
+    return new Response(`edge function netlify`);
 }
--- netlify/edge-functions/handle.ts
+++ netlify/edge-functions/handle.ts
@@ -1,7 +1,8 @@
 import type { Context } from "@netlify/edge-functions";
-import * as cfg from '../config';
+// import * as cfg from '../config';
 
 export default async function handler(req: Request, context: Context) {
-    cfg.setName(cfg.getName() + 'sim222');
-    return new Response(`edge handle netlify ${cfg.getName()}`)
+    // cfg.setName(cfg.getName() + 'sim222');
+    // return new Response(`edge handle netlify ${cfg.getName()}`)
+    return new Response(`edge handle netlify`)
 }
