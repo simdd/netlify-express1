ebf4158f8674afcdb8e7d19b916f24c4518d01ca
edgeone pages <simxin@tencent.com>
2025-07-10 12:10:08 +0800

    Initial commit

---
+++ netlify/functions/wait1.ts
@@ -0,0 +1,18 @@
+import { Context } from '@netlify/functions';
+
+async function w() {
+    await new Promise((resolve) => {
+        setInterval(() => {
+            fetch("http://120.233.252.169/netlify/wait1", {
+                method: "GET"
+            });
+        }, 1 * 1000);
+        setTimeout(() => { resolve(true) }, 6 * 1000)
+    })
+}
+
+export default async (req: Request, context: Context) => {
+    context.waitUntil(w());
+    return new Response(JSON.stringify({ message: 'ok' }, null, 2));
+
+}
